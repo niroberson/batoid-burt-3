62d1d5d6e34a6720f6962578ffc734fe2cad6906
Nathan Roberson <nathaniroberson@gmail.com>
2015-11-17 22:02:08 -0500

    Changing frequency term in waveform

--- src/Arduino/control_test/control_test.ino
+++ src/Arduino/control_test/control_test.ino
@@ -14,16 +14,22 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(); //Default address 0x40
 double offset[] = {1, 17, 10, 15, 7, 3, -11, -12};
 
 // Define x position of servos
-double xs[] = {0, 3, 6, 9, 12, 15, 18, 21};
+double xs[] = {3, 6, 9, 12, 15, 18, 21, 23};
 
 // Define wave parameters
-#define L 23 // Length of the fin
-#define Amax 9/2 // Amplitude (degrees)
-#define f 0.1 // Frequency (Hz)
-#define lam 2.0*L/3.0 // Wavelength
+#define L xs[N-1] // Length of the fin
+#define Amax 12 // Amplitude (degrees)
+#define f 0.25 // Frequency (Hz)
+#define lambda 2.0*L/3.0 // Wavelength
 #define center 90 // Degrees at center line
 
 double start_time = 0;
+// Set up fixed time constants
+int STD_LOOP_TIME = 10;
+int lastLoopTime = STD_LOOP_TIME;
+int lastLoopUsefulTime = STD_LOOP_TIME;
+unsigned long loopStartTime = 0;
+double count = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -34,7 +40,6 @@ void setup() {
 }
 
 void loop() {
-  delay(500);
   double t = millis() - start_time;
   undulate(t);
 }
@@ -50,16 +55,21 @@ void undulate(double t) {
   // Calculate the new position (degrees) of each servo
   for (int iServo = 0; iServo < N; iServo++ ) {
     double A  = xs[iServo]*Amax/L;
-    y[iServo] = A*sin(xs[iServo]*2*M_PI/lam + 1/f*t);
-  }
-  
-  // Set the new position of each servo
-  for(int iServo = 0; iServo < N; iServo++) {
-    Serial.println(y[iServo]);
+    y[iServo] = A*sin(2*M_PI/lambda*xs[iServo]/L - f*t);
     setPosition(iServo, center + y[iServo]);
   }
-   delay(100);
+  fixTime();
 }
+
+void fixTime() {
+  lastLoopUsefulTime = millis() - loopStartTime;
+  if(lastLoopUsefulTime<STD_LOOP_TIME) {
+    delay(STD_LOOP_TIME - lastLoopUsefulTime);
+  }
+  lastLoopTime = millis() - loopStartTime;
+  loopStartTime = millis();
+}
+
 
 void setPosition(int iServo, double angle) {
   double pulse = convertAngleToPulse(iServo, angle);
