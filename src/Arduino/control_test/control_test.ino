acc9dc84ea57d5cff66c63f96a3d360843f69416
Nathan Roberson <nathaniroberson@gmail.com>
2015-11-17 21:02:40 -0500

    Sine wave working

--- src/Arduino/control_test/control_test.ino
+++ src/Arduino/control_test/control_test.ino
@@ -14,13 +14,13 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(); //Default address 0x40
 double offset[] = {1, 17, 10, 15, 7, 3, -11, -12};
 
 // Define x position of servos
-double xs[] = {3, 6, 9, 12, 15, 18, 21, 23};
+double xs[] = {0, 3, 6, 9, 12, 15, 18, 21};
 
 // Define wave parameters
 #define L 23 // Length of the fin
-#define Amax 6 // Amplitude (degrees)
-#define f 2 // Frequency (Hz)
-#define lam 3 // Wavelength
+#define Amax 9/2 // Amplitude (degrees)
+#define f 0.1 // Frequency (Hz)
+#define lam 2.0*L/3.0 // Wavelength
 #define center 90 // Degrees at center line
 
 double start_time = 0;
@@ -34,7 +34,7 @@ void setup() {
 }
 
 void loop() {
-  delay(5000);
+  delay(500);
   double t = millis() - start_time;
   undulate(t);
 }
@@ -46,36 +46,24 @@ void centerFin() {
 }
 
 void undulate(double t) {
-  int y[N];
+  double y[N];
   // Calculate the new position (degrees) of each servo
   for (int iServo = 0; iServo < N; iServo++ ) {
-    int A  = Amax + xs[iServo]*Amax/L;
-    Serial.println(A);
+    double A  = xs[iServo]*Amax/L;
     y[iServo] = A*sin(xs[iServo]*2*M_PI/lam + 1/f*t);
   }
   
   // Set the new position of each servo
   for(int iServo = 0; iServo < N; iServo++) {
-    setPosition(iServo, y[iServo]);
+    Serial.println(y[iServo]);
+    setPosition(iServo, center + y[iServo]);
   }
+   delay(100);
 }
 
 void setPosition(int iServo, double angle) {
   double pulse = convertAngleToPulse(iServo, angle);
   pwm.setPWM(iServo, 0, pulse);  
-}
-
-double getPosition(int iServo) {
-  double pulse = analogRead(iServo);
-  double angle = convertPulseToAngle(iServo, pulse);
-  return angle;
-}
-
-double convertPulseToAngle(int iServo, double pulse) {
-  double pwmrange = SERVOMAX - SERVOMIN;
-  double angrange = 180.0; //Approximate range of servo angle
-  double angle = ((pulse - SERVOMIN) / (pwmrange/angrange)) - offset[iServo];
-  return angle;
 }
 
 double convertAngleToPulse(int iServo, double angle) {
