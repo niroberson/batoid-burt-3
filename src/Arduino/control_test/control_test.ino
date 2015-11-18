32ffc09675c839c5ac81a4b8350cea7e868ef498
Nathan Roberson <nathaniroberson@gmail.com>
2015-11-17 20:48:01 -0500

    Adding in amplitude changing with time

--- src/Arduino/control_test/control_test.ino
+++ src/Arduino/control_test/control_test.ino
@@ -18,40 +18,54 @@ double xs[] = {3, 6, 9, 12, 15, 18, 21, 23};
 
 // Define wave parameters
 #define L 23 // Length of the fin
-#define Amax 9 // Amplitude (degrees)
+#define Amax 6 // Amplitude (degrees)
 #define f 2 // Frequency (Hz)
+#define lam 3 // Wavelength
 #define center 90 // Degrees at center line
 
-int Amax[N]  = Amax[iServo] + xs[iServo]*Amax/L;;
+double start_time = 0;
 
 void setup() {
   Serial.begin(9600);
   pwm.begin();
   pwm.setPWMFreq(60);
   centerFin();
+  start_time = millis();
 }
 
 void loop() {
-  undulate();
+  delay(5000);
+  double t = millis() - start_time;
+  undulate(t);
 }
 
 void centerFin() {
   for (int iServo = 0; iServo < N; iServo++ ) {
-    Amax[iServo] = center;
-    setServoPosition(iServo, Amax[iServo]);
+    setPosition(iServo, center);
   }
 }
 
-void undulate() {
-  setServoPosition(iServo, currentPos[iServo]);
+void undulate(double t) {
+  int y[N];
+  // Calculate the new position (degrees) of each servo
+  for (int iServo = 0; iServo < N; iServo++ ) {
+    int A  = Amax + xs[iServo]*Amax/L;
+    Serial.println(A);
+    y[iServo] = A*sin(xs[iServo]*2*M_PI/lam + 1/f*t);
+  }
+  
+  // Set the new position of each servo
+  for(int iServo = 0; iServo < N; iServo++) {
+    setPosition(iServo, y[iServo]);
+  }
 }
 
-void setServoPosition(int iServo, double angle) {
+void setPosition(int iServo, double angle) {
   double pulse = convertAngleToPulse(iServo, angle);
   pwm.setPWM(iServo, 0, pulse);  
 }
 
-double getServoPosition(int iServo) {
+double getPosition(int iServo) {
   double pulse = analogRead(iServo);
   double angle = convertPulseToAngle(iServo, pulse);
   return angle;
@@ -61,7 +75,6 @@ double convertPulseToAngle(int iServo, double pulse) {
   double pwmrange = SERVOMAX - SERVOMIN;
   double angrange = 180.0; //Approximate range of servo angle
   double angle = ((pulse - SERVOMIN) / (pwmrange/angrange)) - offset[iServo];
-//  int angle =   map(pulse, SERVOMIN, SERVOMAX, 0, 179);
   return angle;
 }
 
