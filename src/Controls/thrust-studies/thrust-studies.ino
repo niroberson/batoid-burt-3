adf9be10491b9d92e74b268a055a216ba298e3d1
Nathan Roberson <nathaniroberson@gmail.com>
2015-12-08 11:31:29 -0500

    Removing temp files

--- src/Controls/thrust-studies/thrust-studies.ino
+++ src/Controls/thrust-studies/thrust-studies.ino
@@ -12,14 +12,15 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(); //Default address 0x40
 
 // Degree offset to center all servo motors
 double offset[] = {1, 17, 10, 15, 7, 3, -11, -12};
-
+//double offset[] = {-12, -11, 3, 7, 15, 10, 17, 1};
 // Define x position of servos, here we have to switch the direction
-double xs[] = {23, 21, 18, 15, 12, 9, 6, 3};
+//double xs[] = {23, 21, 18, 15, 12, 9, 6, 3};
+double xs[] = {3, 6, 9, 12, 15, 18, 21, 23};
 
 // Define wave parameters
-#define L xs[0] // Length of the fin
-#define Amax 12 // Amplitude (degrees)
-#define w 8 // Frequency (rad/s)
+#define L 23 // Length of the fin
+#define Amax 30 // Amplitude (degrees)
+#define w 20 // Frequency (rad/s)
 #define lambda L // Wavelength
 #define center 90 // Degrees at center line
 
