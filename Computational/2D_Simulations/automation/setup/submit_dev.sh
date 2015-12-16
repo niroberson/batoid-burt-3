e0ec2aafb2d4315b810579550ad6269b1e25f1c1
Nathan Irvin Roberson <nathir2@brlogin1.cluster>
2015-12-16 11:33:04 -0500

    Changing directory structure

---
+++ Computational/2D_Simulations/automation/setup/submit_dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -V
+#PBS -l walltime=2:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -W group_list=blueridge
+#PBS -q dev_q
+#PBS -M pid@vt.edu
+#PBS -m bea
+#PBS -A artis71
+cd $PBS_O_WORKDIR
+module load ansys
+fluent 2ddp -t$PBS_NP -pinfiniband -cnf=$PBS_NODEFILE -g -ssh < simulate_fin.tui
